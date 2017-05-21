deb264b759057f18a28a60e992366d932dbdf732
campbell <campbellrehu@me.com>
2017-05-21 15:19:39 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Using Filters in Vue.js v2
+
+## Introduction
+
+This application is a basic implementation of filters in Vue.js v2. The app is based on the tutorial found [here](https://coligo.io/vuejs-filters).
+
+## Thoughts
+
+1. The tutorial was outdated and I believe that Vue.js v1 was being used because when I tried to run the given code, I got an error message saying `Failed to resolve filter: capitalize`. Upon looking at the Vue docs and StackOverflow, I found out that built-in filters are no longer supported in v2. So I had to make my own `capitalize` and `currency` filters and add them to the `filters` object in the Vue component.
+2. Creating the custom filters was a good exercise - especially for the `currency` filter. Having to think about the edge-cases was a valuable task.
+3. I also learned that filters can only be used within the mustache interpolations and `v-bind` expressions. The tutorial called for processing through filters in the `v-for:"product in products"` directive which caused errors when I tried to run the code as well. I had to extract the logic of the `v-for` filters to the Computed properties object.
+4. In the Computed properties object, I added a `filtered()` method which initially returned the filtered products based on the user selection from a `<select></select>` element. 
+5. From this, I added the orderBy `<select></select>` element and implemented the logic in the `filtered()` method.
---
+++ src/App.vue
@@ -0,0 +1,177 @@
+<template>
+  <div id="app">
+    <div class="selector">
+      <label for="filterSelection">Filter by: </label>
+      <select id="filterSelection" v-model="filterSelection">
+        <option value="all">All</option>
+        <option value="electronics">Electronics</option>
+        <option value="accessories">Accessories</option>
+      </select>
+    </div>
+    <div class="selector">
+      <label for="orderBySelection">Order by: </label>
+      <select id="orderBySelection" v-model="orderBySelection">
+        <option value="name">Name</option>
+        <option value="lowToHigh">Lowest to Highest Price</option>
+        <option value="highToLow">Highest to Lowest Price</option>
+      </select>
+    </div>
+    <h1 id="selection">
+      <b>{{filterSelection + " Products" | capitalize}}</b>
+    </h1>
+    <ul>
+      <li v-for="product in filtered">
+        {{product.name | capitalize}} - {{product.price | currency}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  computed: {
+    filtered() {
+      var filterSelection = this.filterSelection;
+      var orderBySelection = this.orderBySelection;
+      var filteredProducts = this.products;
+
+      if (filterSelection != 'all') {
+        filteredProducts = filteredProducts.filter((product) => {
+          return product.category === filterSelection;
+        });
+      }
+
+      if (orderBySelection === 'lowToHigh') {
+          return filteredProducts.sort((productA, productB) => {
+            var priceA = parseFloat(productA.price);
+            var priceB = parseFloat(productB.price);
+            if (priceA > priceB) {
+              return 1;
+            } else if (priceA < priceB) {
+              return -1;
+            }
+            return 0;
+          });
+      } else if (orderBySelection === 'highToLow') {
+        return filteredProducts.sort((productA, productB) => {
+          var priceA = parseFloat(productA.price);
+          var priceB = parseFloat(productB.price);
+          if (priceB > priceA) {
+            return 1;
+          } else if (priceB < priceA) {
+            return -1;
+          }
+          return 0;
+        });
+      } else {
+        return filteredProducts.sort((productA, productB) => {
+          var nameA = productA.name.toLowerCase();
+          var nameB = productB.name.toLowerCase();
+          
+          if (nameA > nameB) {
+            return 1;
+          } else if (nameA < nameB) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+      return filteredProducts;
+    }
+  },
+  filters: {
+    capitalize(value) {
+      if (!value) return '';
+      
+      value = value.toString();
+      
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+    currency(value) {
+      if (!value) {
+        return '';
+      } else if (isNaN(value)) { // Check that the value is a number
+        return 'POA';
+      }
+      value = value.toString();
+
+      // Find the decimal place in the value
+      var decimalPlace = value.indexOf('.');
+
+      // If there is no decimal place, add a decimal place and two zeroes
+      if (decimalPlace === -1) {
+        value += '.00';
+      } else {
+        // Find the number of digits after the decimal place
+        var postDecimalDigits = value.slice(decimalPlace, value.length-1).length;
+        
+        // If there are more than two digits after the decimal place, round it to two.
+        if ( postDecimalDigits > 2 ) {
+          
+          /* Find the number of digits before the decimal place.
+            This is to use in the toPrecision() method which needs
+            to know the total number of digits in the number. 
+          */
+          var preDecimalDigits = value.slice(0, decimalPlace).length;
+          var floatValue = parseFloat(value).toPrecision(preDecimalDigits+2);
+          value = floatValue;
+        }
+      }
+
+      // Add the dollar sign
+      return '$' + value;
+    }
+  },
+  data() {
+    return {
+      filterSelection: 'all',
+      orderBySelection: 'name',
+      products: [
+        {
+          name: 'microphone', price: '25', category: 'electronics'
+        },
+        {
+          name: 'laptop case', price: '15', category: 'accessories'
+        },
+        {
+          name: 'screen cleaner', price: '17', category: 'accessories'
+        },
+        {
+          name: 'laptop charger', price: '70', category: 'electronics'
+        },
+        {
+          name: 'mouse', price: '40', category: 'electronics'
+        },
+        {
+          name: 'earphones', price: '20', category: 'electronics'
+        },
+        {
+          name: 'monitor', price: '120', category: 'electronics'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.selector {
+  margin: 20px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
